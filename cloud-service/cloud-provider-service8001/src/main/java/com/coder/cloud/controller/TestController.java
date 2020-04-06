b6d5bc6af2681d9e967d63df679e38df93e55241
CRUDCODER <710683598@qq.com>
2020-04-06 18:50:59 +0800

    新增providerTest2方法 测试超时

--- cloud-service/cloud-provider-service8001/src/main/java/com/coder/cloud/controller/TestController.java
+++ cloud-service/cloud-provider-service8001/src/main/java/com/coder/cloud/controller/TestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author liujin
  * @date created in 2020/4/6 16:03
@@ -21,6 +23,16 @@ public class TestController {
     private String applicationName;
     @GetMapping("/get")
     public CommonResult<String> providerTest(){
+        return new CommonResult(200,"访问成功","applicationName:"+applicationName+",i from port:"+serverPort);
+    }
+
+    @GetMapping("/get2")
+    public CommonResult<String> providerTest2(){
+        try {
+            TimeUnit.SECONDS.sleep(7);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return new CommonResult(200,"访问成功","applicationName:"+applicationName+",i from port:"+serverPort);
     }
 }
