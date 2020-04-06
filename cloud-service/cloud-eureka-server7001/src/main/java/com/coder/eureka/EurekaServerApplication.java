6098715c27b2bfef04de4303cb6ff6d6f8ff6b3a
CRUDCODER <710683598@qq.com>
2020-04-06 16:15:01 +0800

    分布式服务注册中心

---
+++ cloud-service/cloud-eureka-server7001/src/main/java/com/coder/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.coder.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author liujin
+ * @date created in 2020/4/6 15:56
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
